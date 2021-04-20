6fe5a4bd38cd75a35bc59ee9fe2ec01530a5a648
sdominguezr <s.dominguezr.2020@alumnos.urjc.es>
2021-04-20 18:54:33 +0200

    Añadir textos de genoma Añadido practice 0 Avance en ejercicio 8 rehehco Terminado la parte 5 del ejercicio 8, sin poder hacer funciones aun Añadido la parte main del ejercicio 8 rehecho Añadido los ejercicios de P01, todos Cambiado el formato de P01 Añadido el directorio Session05 Añadido a P1 ejercicio 1,2,4 y Seq1 Creado un subdirectorio de la Session06 en la Session05 Creado el directorio de la Session07, añadido el documento Theory al directorio Practica 2 Clase practica 07/04. Ejercicios de teoria + comienzo de practica 3 Session12 Practica 3 excepto 7 Practica 3 entera hecha Session 14 teoria y ejercicios de teoría

--- P3/new_server.py
+++ P3/new_server.py
@@ -4,38 +4,31 @@ from P3 import server_utilities
 
 list_sequences = ["U5" , "ADA","FRAT1",   "FXN", "RNU6_269P"]
 
-# Configure the Server's IP and PORT
+
 PORT = 8081
 IP = "127.0.0.1"  # Esto significa a que unicamente funcionará para aquellos programas que esten en la misma maquina
 # IP se puede quedar en blanco tambien
-# -- Step 1: create the socket
+
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# -- Step 2: Bind the socket to server's IP and PORT
+
 ls.bind((IP, PORT))
 count_connection = 0
-# -- Step 3: Configure the socket for listening
 ls.listen()
 client_address_list =[]
 count = +1
 print("The server is configured")
 while True:
-    # -- Waits for a client to connect
     print("Waiting for Clients to connect")
     try:
         (cs, client_ip_port) = ls.accept()
         count_connection =+ 1
         print("Connection " + str(count_connection) + ". Client IP, PORT: " + str(client_ip_port))
-    # -- Server stopped manually
+
     except KeyboardInterrupt:
         print("Server stopped by the user")
-        # -- Close the listenning socket
         ls.close()
-        # -- Exit!
         exit()
-    # -- Execute this part if there are no errors
     print("A client has connected to the server!")
-    # -- Read the message from the client
-    # -- The received message is in raw bytes
     msg_raw = cs.recv(2048)
     print(msg_raw)
     msg = msg_raw.decode()
