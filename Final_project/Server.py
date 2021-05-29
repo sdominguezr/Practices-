e476a84a50ce3c123f84d37ae7fbb25fc04566f3
sdominguezr <s.dominguezr.2020@alumnos.urjc.es>
2021-05-29 22:02:22 +0200

    Casi practica 6, 28/04
    Basic level hecho, repasar karyotype

--- Final_project/Server.py
+++ Final_project/Server.py
@@ -22,41 +22,46 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         # We are NOT processing the client's request
         # It is a happy server: It always returns a message saying
         # that everything is ok
-
-        if self.path == "/" or self.path == '/main_page.html':
-            contents = Functions.read_html_file("./html/main_page.html")
-        elif "/limit_sequence" in self.path:
-            query_params = self.path.split('?')[1]
-            limit = int(parse_qs(query_params)['limit'][0])
-            species_len, common_names = Functions.get_info_species(limit)
-            format_parameters = {'species_len': species_len, 'limit': limit, 'common_names': common_names}
-            contents = Functions.read_template_html_file("./html/limit_sequence.html", **format_parameters)
-        elif "/karyotype" in self.path:
-            query_params_kariotype = self.path.split('?')[1]
-            name_specie = (parse_qs(query_params_kariotype)["karyotype"][0])
-            result_karyotype = Functions.get_karyotype(name_specie)
-            format_parameters_karyo = {'karyotype': result_karyotype}
-            contents = Functions.read_template_html_file("./html/karyotype.html", **format_parameters_karyo)
-        elif '/chromosome_length' in self.path:
-            query_params_n = self.path.split('?')[1].split('&')[0] #number of chromosome value
-            query_params_s = self.path.split('?')[1].split('&')[1] #specie
-            specie = parse_qs(query_params_s)['specie'][0]
-            number_of_chromosome = int(parse_qs(query_params_n)["length"][0])
-            result_number_of_chromosome = str(Functions.get_length_chromosome(specie, number_of_chromosome))
-            format_parameter = {'length': result_number_of_chromosome}
-            contents = (Functions.read_template_html_file('./html/chromosome_length.html', **format_parameter))
-            #contents = Functions.read_html_file("./html/info/G.html")
-        elif self.path == "/info/T":
-            contents = Functions.read_html_file("./html/info/T.html")
-        elif self.path.endswith((".html")):
-            try:
-                contents = Functions.read_html_file(".html" + self.path)
-            except FileNotFoundError:
+        try:
+            if self.path == "/" or self.path == '/main_page.html':
+                contents = Functions.read_html_file("./html/main_page.html")
+            elif "/limit_sequence" in self.path:
+                query_params = self.path.split('?')[1]
+                limit = int(parse_qs(query_params)['limit'][0])
+                species_len, common_names = Functions.get_info_species(limit)
+                format_parameters = {'species_len': species_len, 'limit': limit, 'common_names': common_names}
+                contents = Functions.read_template_html_file("./html/limit_sequence.html", **format_parameters)
+            elif "/karyotype" in self.path:
+                query_params_kariotype = self.path.split('?')[1]
+                name_specie = (parse_qs(query_params_kariotype)["karyotype"][0])
+                result_karyotype = Functions.get_karyotype(name_specie)
+                format_parameters_karyo = {'karyotype': result_karyotype}
+                contents = Functions.read_template_html_file("./html/karyotype.html", **format_parameters_karyo)
+            elif '/chromosome_length' in self.path:
+                query_params_n = self.path.split('?')[1].split('&')[0] #number of chromosome value
+                query_params_s = self.path.split('?')[1].split('&')[1] #specie
+                specie = parse_qs(query_params_s)['specie'][0]
+                number_of_chromosome = int(parse_qs(query_params_n)["length"][0])
+                result_number_of_chromosome = str(Functions.get_length_chromosome(specie, number_of_chromosome))
+                format_parameter = {'length': result_number_of_chromosome}
+                contents = (Functions.read_template_html_file('./html/chromosome_length.html', **format_parameter))
+                #contents = Functions.read_html_file("./html/info/G.html")
+            elif self.path == "/info/T":
+                contents = Functions.read_html_file("./html/info/T.html")
+            elif self.path.endswith((".html")):
+                try:
+                    contents = Functions.read_html_file(".html" + self.path)
+                except FileNotFoundError:
+                    contents = Functions.read_html_file("./html/error.html")
+            else:
                 contents = Functions.read_html_file("./html/error.html")
-        else:
-            contents = Functions.read_html_file("./html/error.html")
         # Message to send back to the clinet
-
+        except KeyError:
+            contents = Functions.read_html_file("./html/error.html")
+        except ValueError:
+            contents = Functions.read_html_file("./html/error.html")
+        except TypeError:
+            contents =Functions.read_html_file('./html/error.html')
 
         # Generating the response message
         self.send_response(200)  # -- Status line: OK!
